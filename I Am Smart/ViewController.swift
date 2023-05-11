ae8ffba3c9b1182a5264e1425cd374c0d589648e
taha murat <ttahamuratdoser@gmail.com>
2023-05-11 20:11:42 +0300

    Initial Commit

---
+++ I Am Smart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Smart
+//
+//  Created by Taha  on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
